ff08c452be52c6016967bd9e6933966ad54aa9a3
chillhaze <alexander.gotra@gmail.com>
2021-09-03 02:16:05 +0300

    fixed statistics h2 render function

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -5,7 +5,7 @@ import css from './Statistics.module.css';
 export const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
-      {title ? <h2 className={css.title}>{title}</h2> : ''}
+      {title && <h2 className={css.title}>{title}</h2>}
 
       <ul className={css.statList}>
         {stats.map(({ id, label, percentage }) => {
